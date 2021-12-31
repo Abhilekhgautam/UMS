cc39634a2fec12a25d6367562073d556e577b0c9
Abhilekh <abhilekhgautam119@gmail.com>
2022-01-01 01:21:00 +0545

    Added click handlers for the search button

--- searchframe.cpp
+++ searchframe.cpp
@@ -5,9 +5,13 @@
 
 searchFrame :: searchFrame()
              : wxFrame(nullptr, wxID_ANY, "" , wxDefaultPosition, wxSize(240, 250), wxDEFAULT_FRAME_STYLE | wxMAXIMIZE){
-
+  /*
+     TODO:1. Create a good UI
+          2. define a drawTable function
+          3. Create a button which on click opens the addUser dialog
+  */
   wxGridSizer* my_sizer = new wxGridSizer(1, 4, 0, 1);
-
+// Search box
  wxTextCtrl* text_box = new wxTextCtrl(this, -1, wxEmptyString, wxDefaultPosition, wxSize(400, 30), wxTE_CAPITALIZE | wxTE_PROCESS_ENTER);
 
  wxButton* search_button = new wxButton(this, wxID_ANY, "Search", wxDefaultPosition, wxDefaultSize);
@@ -25,3 +29,21 @@ searchFrame :: searchFrame()
 
 }
 
+wxBEGIN_EVENT_TABLE(searchFrame, wxFrame)
+  EVT_BUTTON(1, searchFrame::onSearchClick)
+  EVT_MENU(wxID_EXIT, searchFrame::onExit)
+wxEND_EVENT_TABLE()
+
+void searchFrame::onSearchClick(wxCommandEvent& event){
+ /*
+   TODO: call the drawTable function
+ */
+
+}
+
+void searchFrame::onExit(wxCommandEvent& event){
+ wxMessageDialog* alert = new wxMessageDialog(this, wxT("Are you sure you want to Exit"), wxT("EXIT"), wxOK | wxCANCEL);
+  if(alert->ShowModal() ==  wxOK){
+    Close(true);
+ }
+}
--- searchframe.h
+++ searchframe.h
@@ -2,6 +2,11 @@
 #include <wx/wx.h>
 class searchFrame : public wxFrame{
 
+ private:
+  void onSearchClick(wxCommandEvent& event);
+  void onExit(wxCommandEvent& event); 
+  wxDECLARE_EVENT_TABLE();
+
  public:
   searchFrame();
   void drawTable(int row, int col);
