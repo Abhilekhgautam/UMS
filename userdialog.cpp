c5cdcb9e7423e97c95e54ec3bbe3273fa7a55af5
Abhilekh <abhilekhgautam119@gmail.com>
2022-01-01 01:20:23 +0545

    Implemented the data validation of the input field except for image

--- userdialog.cpp
+++ userdialog.cpp
@@ -15,29 +15,29 @@ userDialog::userDialog(const wxString& Title)
  wxStaticBox* name = new wxStaticBox(panel, -1, wxT("Full Name"), wxPoint(15,5), wxSize(250,100));
 
  wxStaticBox* image = new wxStaticBox(panel, -1, wxT("Image"), wxPoint(280, 5), wxSize(200,200));
- wxButton* add_img = new wxButton(panel, 1, wxT("Select"), wxPoint(340, 80), wxSize(70,30)); 
+ imagePanel = new wxPanel(this, -1, wxPoint (285, 25),wxSize(190,175));
  
 
- wxTextCtrl* name_text = new wxTextCtrl(panel, -1, wxEmptyString,  wxPoint(20,20), wxSize(230, 80));
+ name_text = new wxTextCtrl(panel, -1, wxEmptyString,  wxPoint(20,20), wxSize(230, 80));
 
  wxStaticBox* email = new wxStaticBox(panel,-1,wxT("Email"),wxPoint(15, 105), wxSize(250,100));
- wxTextCtrl* email_text = new wxTextCtrl(panel, -1, wxEmptyString,  wxPoint(20,120), wxSize(230, 80));
+ email_text = new wxTextCtrl(panel, -1, wxEmptyString,  wxPoint(20,120), wxSize(230, 80));
 
 
  wxStaticBox* address = new wxStaticBox(panel,-1,wxT("Address"),wxPoint(15, 205),wxSize(250,100));
- wxTextCtrl* address_text = new wxTextCtrl(panel, -1, wxEmptyString,  wxPoint(20,220), wxSize(230, 80));
+address_text = new wxTextCtrl(panel, -1, wxEmptyString,  wxPoint(20,220), wxSize(230, 80));
 
  wxStaticBox* phone = new wxStaticBox(panel, -1 , wxT("Phone"), wxPoint(280, 205 ), wxSize(250,100));
 
- wxTextCtrl* phone_text = new wxTextCtrl(panel, -1, wxEmptyString, wxPoint(290, 220), wxSize(230,80)); 
+phone_text = new wxTextCtrl(panel, -1, wxEmptyString, wxPoint(290, 220), wxSize(230,80)); 
 
  wxStaticBox* gender = new wxStaticBox(panel, -1, wxT("Gender"), wxPoint(15, 305), wxSize(250,100));
   
-  wxRadioButton* gen_male = new wxRadioButton(panel, -1, 
+ gen_male = new wxRadioButton(panel, -1, 
       wxT("Male"), wxPoint(15, 325));
-  wxRadioButton* gen_female = new wxRadioButton(panel, -1, 
+ gen_female = new wxRadioButton(panel, -1, 
       wxT("Female"), wxPoint(15, 345));
-  wxRadioButton *gen_others = new wxRadioButton(panel, -1, 
+ gen_others = new wxRadioButton(panel, -1, 
       wxT("Others"), wxPoint(15, 365));
 
 
@@ -77,16 +77,69 @@ if(dialog->ShowModal() == wxID_OK)
 }
 
 void userDialog::onAddUser(wxCommandEvent& event){
- wxMessageDialog* dialog = new wxMessageDialog(this, wxT("A new user will be added"), wxT("Question"), wxOK | wxCANCEL);
- dialog->ShowModal();
+/*
+  TODO:1.Check if all the textbox are filled
+       2.Minor check for email 'check for @ and . symbol'
+       3.Check the length of phone number
+*/
+
+//check if all text box are filled at least!
+  if(name_text->GetLineLength(0) < 1 || email_text->GetLineLength(0) < 1 || address_text->GetLineLength(0) < 1){
+    wxMessageDialog* error_dialog = new wxMessageDialog(this, wxT("All the input fields are necessary"), wxT("Error"), wxOK | wxICON_ERROR);
+    error_dialog->ShowModal();
+    return;
+ }
+
+ //check if email has a '@' and '.' symbol
+  wxString check_email =  email_text->GetLineText(0);
+  int count = 0;
+  int len = 0;
+ for(char elt : check_email){
+   if(elt == '@' || elt == '.') count ++;
+   len ++;
+ }
+ 
+ if(count != 2 || len < 8 ){
+  wxMessageDialog* error_dialog = new wxMessageDialog(this, wxT(" Your email is invalid "), wxT("Error"), wxOK | wxICON_ERROR);
+    error_dialog->ShowModal();
+    return;
+
+ }
+
+//check if phone number has 10 digits
+/*
+  TODO: Check if all the input characters are integer
+*/
+  if(phone_text->GetLineLength(0) != 10){
   
+ wxMessageDialog* error_dialog = new wxMessageDialog(this, wxT(" Your phone number is invalid "), wxT("Error"), wxOK | wxICON_ERROR);
+    error_dialog->ShowModal();
+    return;
+
+ }  
+// Informs the user that if everything goes well a new user will be added
+ wxMessageDialog* dialog = new wxMessageDialog(this, wxT("A new user will be added"), wxT("Question"), wxOK | wxCANCEL);
+ if(dialog->ShowModal() == wxOK){
+  /*
+    TODO: 1. Call the User's C'tor to create a new user
+          2. Add the new data to the file
+          3. Check if the data is successfully added to the file in case of failure inform the user about failure
+          4. Informs the user that a user has been successfully added to the file
+          5. Clear all the input field
+  */ 
+ } 
 }
 
 void userDialog::onSelectImage(wxCommandEvent& event){
- //to do: view the selected image
+
  
- wxFileDialog* selectFile = new wxFileDialog(this, "Select an Image","","", "*.png");
- 
- 
- selectFile->ShowModal();
+   wxFileDialog* selectFile = new wxFileDialog(this, "Select an Image","","", "*.png");
+   if(selectFile->ShowModal() == 5100){
+     wxString file_path = selectFile->GetPath();
+    /*
+        
+       TODO: display the selected image
+   */
+  }
+   
 }
