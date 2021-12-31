5c0ad8d29d818afaa8bdc13e80c5fd9243f99a6d
Abhilekh <abhilekhgautam119@gmail.com>
2022-01-01 01:19:22 +0545

    added text box and radio buttons

--- userdialog.h
+++ userdialog.h
@@ -7,9 +7,16 @@ class userDialog : public wxDialog{
   void onAddUser(wxCommandEvent& event);
   void onSelectImage(wxCommandEvent& event);
   void onCancel(wxCommandEvent& event);
-
+  wxTextCtrl* name_text;
+  wxTextCtrl* email_text;
+  wxTextCtrl* address_text;
+  wxTextCtrl* phone_text;
+  wxRadioButton* gen_male;
+  wxRadioButton* gen_female;
+  wxRadioButton* gen_others;
   wxDECLARE_EVENT_TABLE();
  public:
   userDialog(const wxString &Title);
+  wxPanel* imagePanel;  
 
 };
