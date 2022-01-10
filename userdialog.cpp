d8806577b71cf337673e76183a5d97e393614084
Abhilekh <abhilekhgautam119@gmail.com>
2022-01-11 00:32:10 +0545

    event handling for addUser button added

--- userdialog.cpp
+++ userdialog.cpp
@@ -1,3 +1,4 @@
+//#include "User.h"
 #include <wx/image.h>
 #include <wx/valnum.h>
 #include "userdialog.h"
@@ -11,21 +12,26 @@ userDialog::userDialog(const wxString& Title, wxWindow* parent)
  pp.LoadFile(wxT("./media/default_pp.jpg"));
  unsigned long m_value = 98; 
 
+//Validator for the phone number input
   wxIntegerValidator<unsigned long>
    val(&m_value, wxNUM_VAL_THOUSANDS_SEPARATOR);
 
-  panel = new wxPanel(this, -1);
+ panel = new wxPanel(this, -1);
  wxBoxSizer* vbox = new wxBoxSizer(wxVERTICAL);
  wxBoxSizer* hbox = new wxBoxSizer(wxHORIZONTAL);
 
+// Input fields start here
  wxStaticBox* name = new wxStaticBox(panel, -1, wxT("Full Name"), wxPoint(15,5), wxSize(250,100));
 
+//select image interface
  image = new wxStaticBox(panel, -1, wxT("Image"), wxPoint(280, 5), wxSize(200,230));
  profile = new wxStaticBitmap (panel, wxID_ANY, pp, wxPoint(280,25),wxSize(200,190));
  wxButton* select = new wxButton(panel ,1, wxT("Select"), wxPoint(285, 215), wxSize(70,30));
  wxButton* set_default = new wxButton(panel , 4, wxT("Set default"), wxPoint(365, 215), wxSize(90,30));
 
+// name input field
  name_text = new wxTextCtrl(panel, -1, wxEmptyString,  wxPoint(20,20), wxSize(230, 80));
+
 
  wxStaticBox* email = new wxStaticBox(panel,-1,wxT("Email"),wxPoint(15, 105), wxSize(250,100));
  email_text = new wxTextCtrl(panel, -1, wxEmptyString,  wxPoint(20,120), wxSize(230, 80));
@@ -40,6 +46,8 @@ phone_text = new wxTextCtrl(panel, -1, wxEmptyString, wxPoint(290, 245), wxSize(
 
  wxStaticBox* gender = new wxStaticBox(panel, -1, wxT("Gender"), wxPoint(15, 305), wxSize(250,100));
   
+
+//Radio button for gender Selection
  gen_male = new wxRadioButton(panel, -1, 
       wxT("Male"), wxPoint(15, 325));
  gen_female = new wxRadioButton(panel, -1, 
@@ -47,6 +55,7 @@ phone_text = new wxTextCtrl(panel, -1, wxEmptyString, wxPoint(290, 245), wxSize(
  gen_others = new wxRadioButton(panel, -1, 
       wxT("Others"), wxPoint(15, 365));
 
+//Input field ends here
 
 
  wxButton* addButton = new wxButton(this, 2, wxT("Add"), wxDefaultPosition, wxSize(70, 30));
@@ -56,12 +65,16 @@ phone_text = new wxTextCtrl(panel, -1, wxEmptyString, wxPoint(290, 245), wxSize(
  hbox->Add(addButton, 1);
  hbox->Add(cancelButton, 1, wxLeft, 5);
 
+//Add the panel to the vertical sizer
  vbox->Add(panel, 1);
  vbox->Add(hbox, 0 , wxALIGN_CENTER | wxTOP | wxBOTTOM , 10); 
 
  SetSizer(vbox);
 
+//Center the dialogbox
  Centre();
+
+//display the dialogbox
  ShowModal();
  
  Destroy();
@@ -76,12 +89,15 @@ wxBEGIN_EVENT_TABLE(userDialog, wxDialog)
    EVT_CLOSE(userDialog::onQuit)
  wxEND_EVENT_TABLE()
 
+//Sets image to the default profile picture
+
 void userDialog::onSetDefault(wxCommandEvent& event){
 
  setProfile("./media/default_pp.jpg");
 
 }
 
+//Changes the profile Image of the User
 void userDialog::setProfile(wxString filename){
  pp = wxBitmap(wxSize(200,190));
  if(pp.LoadFile(filename)){
@@ -96,6 +112,7 @@ void userDialog::setProfile(wxString filename){
 
 }
 
+//Event handler for the exit event
 void userDialog::onQuit(wxCloseEvent& event){
  
  wxMessageDialog* quitWarn = new wxMessageDialog(this, wxT("No user will be added,Do you want to Continue?"),wxT("Exit"), wxOK | wxCANCEL);
@@ -166,10 +183,40 @@ void userDialog::onAddUser(wxCommandEvent& event){
 //*conversion from wxString to string
   /*std::string name = std::string(name_text->GetLineText(0).mb_str());
   std::string email =  std::string(email_text->GetLineText(0).mb_str());
-  std::string address = std::string(address->text->GetLineText(0).mb_str());
+  std::string address = std::string(address_text->GetLineText(0).mb_str());
+  std::string phone = std::string(phone_text->GetLineText(0).mb_str());
+  std::string gender = "male"
    */
+
+/*
+ //call the function to add to the database
+ 
+ // this call should return a success or failure message 
+  bool success = addUser("test_id",name, email, address, phone , gender)
+
+   if(!success){
+    
+   wxMessageDialog* warning = new wxMessageDialog(this, "Cannot add User", "Error", wxOK);
+   if(warning->ShowModal() == wxID_OK)
+     return;
+  }
+
+  else{
+    wxMessageDialog* success = new wxMessageDialog(this, "User Added Successfully", "Success", wxOK);
+       if(success->ShowModal() == wxID_OK){
+        //destroys the addUser dialog
+         Destroy()
+    }  
+   }
+   
+*/
  } 
 }
+
+/*
+  Responsible for the image selection process from the device 
+
+*/
 
 void userDialog::onSelectImage(wxCommandEvent& event){
 
@@ -177,11 +224,10 @@ void userDialog::onSelectImage(wxCommandEvent& event){
    wxFileDialog* selectFile = new wxFileDialog(this, "Select an Image","","", "`PNG and JPEG files (*.png; *jpg)|*.png;*.jpg");
   if(selectFile->ShowModal() == 5100){
      wxString file_path = selectFile->GetPath();
-    /*
-        
-       
-   Function: displays the selected image
-   */
+    
+
+  //Provides the preview selected image feature
+   
   wxBitmap pp;
  if(pp.LoadFile(file_path)){
    if (pp.GetHeight() > 210 || pp.GetWidth() > 210){
