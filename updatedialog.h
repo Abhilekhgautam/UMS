b31ac3e3816456666a91a83d41569a9830122b15
Abhilekh <abhilekhgautam119@gmail.com>
2022-01-13 17:13:46 +0545

    dialogbox which updates the user

---
+++ updatedialog.h
@@ -0,0 +1,29 @@
+#include "userdialog.h"
+#include <wx/wx.h>
+
+//A class just for the simulation of database connection
+#ifndef testUser_h
+  #define testUser_h
+
+    class testUser{
+      public:
+     wxString id, name, gender, email, phone, address, path;
+
+       testUser();
+       testUser(wxString i, wxString n, wxString g, wxString e, wxString p, wxString add, wxString image_path);
+
+    };
+
+
+#endif
+
+class updateDialog : public userDialog{
+
+virtual void onBtnClick(wxCommandEvent& event);
+void onCancel(wxCommandEvent& event);
+
+ public:
+  updateDialog(wxWindow* parent,wxString Title, testUser&);//TODO:change the third parameter to backend's class equivalent to testUser
+  void showDialog();
+
+};
