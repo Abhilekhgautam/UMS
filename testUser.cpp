38ce815f7d42640dabbc949cc52ba69cd39d068d
Abhilekh <abhilekhgautam119@gmail.com>
2022-01-13 17:19:44 +0545

    Class to simulate backend action, will be deleted in upcoming commits

---
+++ testUser.cpp
@@ -0,0 +1,14 @@
+#include "testUser.h"
+
+testUser :: testUser(){}
+
+testUser :: testUser(wxString i, wxString n, wxString g, wxString e, wxString p, wxString add, wxString image_path){
+
+       id = i; 
+       name = n; 
+       gender = g; 
+       email = e; 
+       phone = p; 
+       address = add;
+       path = image_path;
+}
