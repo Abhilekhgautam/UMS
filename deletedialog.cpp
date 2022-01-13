c5e0f939c1b27769201df555b4a0cb41f369b369
Abhilekh <abhilekhgautam119@gmail.com>
2022-01-13 17:17:58 +0545

    dialogbox to delete users

---
+++ deletedialog.cpp
@@ -0,0 +1,41 @@
+#include "wx/wx.h"
+#include "deletedialog.h"
+
+#ifndef testUser_h
+  #define testUser_h
+
+    class testUser{
+      public:
+     wxString id, name, gender, email, phone, address, path;
+
+       testUser();
+       testUser(wxString i, wxString n, wxString g, wxString e, wxString p, wxString add, wxString image_path);
+
+    };
+
+
+#endif
+
+deleteDialog :: deleteDialog (wxWindow* parent, testUser& usr, wxString title)
+              : updateDialog(parent, title,  usr){}
+
+void deleteDialog :: showDialog(){
+
+ ShowModal();
+
+}
+
+void deleteDialog :: onButtonClick(wxCommandEvent& event){
+
+  wxMessageDialog* message = new wxMessageDialog(this, "Delete this User?","Confirm",wxOK | wxCANCEL);
+  if(message -> ShowModal() == wxID_OK)
+    Destroy();
+  
+}
+
+void deleteDialog :: onCancel(wxCommandEvent& event){
+
+ wxMessageDialog* warning = new wxMessageDialog(this, "No User will be deleted", "Confirm", wxOK | wxCANCEL);
+ warning->ShowModal();
+  Destroy();
+}
---
+++ deletedialog.h
@@ -0,0 +1,31 @@
+#include <wx/wx.h>
+#include "updatedialog.h"
+
+
+//This is just a test class to simulate database connection
+
+#ifndef testUser_h
+  #define testUser_h
+
+    class testUser{
+      public:
+     wxString id, name, gender, email, phone, address, path;
+
+       testUser();
+       testUser(wxString i, wxString n, wxString g, wxString e, wxString p, wxString add, wxString image_path);
+
+    };
+
+
+#endif
+
+
+class deleteDialog : public updateDialog{
+
+  void onButtonClick(wxCommandEvent& event);
+  void onCancel(wxCommandEvent& event);
+
+ public:
+   deleteDialog(wxWindow* parent, testUser& usr, wxString title);//TODO: change the second parameter to backend class which is equivalent to testUser
+   void showDialog();
+};
