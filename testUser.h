e222d65c9cf58be7420726d6458eac6ce5bfd489
Abhilekh <abhilekhgautam119@gmail.com>
2022-01-13 17:18:46 +0545

    Class to simulate backend action, will be deleted in upcoming commit

---
+++ testUser.h
@@ -0,0 +1,10 @@
+#include <wx/wx.h>
+
+class testUser{
+ public:
+  wxString id, name, gender, email, phone, address, path;
+ 
+    testUser();
+    testUser(wxString i, wxString n, wxString g, wxString e, wxString p, wxString add, wxString image_path);
+ 
+ };
